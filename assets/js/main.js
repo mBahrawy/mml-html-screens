a75fac1434dd53a91e1f1d784cc95ac298dec359
mBahrawy <bahrawy.dev@gmail.com>
2018-09-17 23:09:23 -0700

    SP profile edit page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 // Avoid `console` errors in browsers that lack a console.
-(function () {
+(function() {
   var method;
-  var noop = function () {};
+  var noop = function() {};
   var methods = [
     'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
     'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
@@ -28,7 +28,7 @@
 /*================== SmartMenus jQuery init/*================== */
 
 // SmartMenus init
-$(function () {
+$(function() {
   $('#main-menu').smartmenus({
     mainMenuSubOffsetX: -1,
     mainMenuSubOffsetY: 4,
@@ -57,7 +57,7 @@ function pcView() {
 
 
 function changeNavPosition() {
-  if ($(window).width() < 1071) {
+  if ($(window).width() < 992) {
 
     //tab
     navContainerForMobile.appendChild(mainNavWrapper);
@@ -72,31 +72,25 @@ function changeNavPosition() {
 
 changeNavPosition();
 
-window.onresize = function () {
+window.onresize = function() {
   changeNavPosition();
 
 }
 
 
 
-$(".toggle-mobile-nav-btn").click(function () {
+$(".toggle-mobile-nav-btn").click(function() {
 
   if ($state == false) {
-    $(".main-nav").slideDown("slow", function () {});
+    $(".main-nav").slideDown("slow", function() {});
     $state = true;
     $(".toggle-mobile-nav-btn").removeClass("collabsed").addClass("opend");
 
   } else {
-    $(".main-nav").slideUp("slow", function () {});
+    $(".main-nav").slideUp("slow", function() {});
     $state = false;
     $(".toggle-mobile-nav-btn").removeClass("opend").addClass("collabsed");
-
-
-
   }
-
-
-
 });
 
 
@@ -105,13 +99,14 @@ $(".toggle-mobile-nav-btn").click(function () {
 // Image upload
 // ==========================================================
 
-$(function(){
+$(function() {
   // For image picker
-  $("#image-picker").change(function(){
+  $("#image-picker").change(function() {
+
     var file = this.files[0],
       reader = new FileReader(),
       img = $(this).siblings('img.picked-image')
-    reader.onload = function (e) {
+    reader.onload = function(e) {
       img.attr('src', e.target.result);
     }
     reader.readAsDataURL(file);
@@ -120,19 +115,23 @@ $(function(){
   // For checking file size
   var fileInput = $('#image-picker');
   var maxSize = fileInput.data('max-size');
-  var maxSizeInBytes = maxSize*1000000;
+  var maxSizeInBytes = maxSize * 1000000;
 
-  $('form').submit(function(e){
+  $('form').submit(function(e) {
 
-    
-    if( $('form').find('input[type="file"]').length > 0){
 
-      if(fileInput.get(0).files.length){
-          var fileSize = fileInput.get(0).files[0].size; // in bytes
-          if(fileSize>maxSizeInBytes){
-              $('#image-picker+span').html('Your photo size is more than '+maxSize+' MB, please select another one');
-              return false;
-          }
+    if ($('form').find('input[type="file"]').length > 0) {
+
+      if (fileInput.get(0).files.length) {
+        var fileSize = fileInput.get(0).files[0].size; // in bytes
+        if (fileSize > maxSizeInBytes) {
+
+          $('#image-picker+span').html('Your photo size is more than ' + maxSize + ' MB, please select another one');
+          return false;
+        } else {
+          $('#image-picker+span').html('');
+
+        }
       }
     }
 
@@ -141,22 +140,37 @@ $(function(){
 
 
 
-
-
-// Equipments  SP edit profile page
+// Add new Equipment  Expand & Collapse 
 // ==========================================================
 
-$('button.expand').click(function(e){
- e.preventDefault();
-  $('div.collapse-container').css("max-height","500px");
+$('button.expand').click(function(e) {
+  e.preventDefault();
+  $('div.collapse-container').css("max-height", "500px");
   $('button.collapse').show();
 })
 
-$('button.collapse').click(function(e){
+$('button.collapse').click(function(e) {
   e.preventDefault();
   $(this).hide();
-  $('div.collapse-container').css("max-height","0");
+  $('div.collapse-container').css("max-height", "0");
 })
+
+
+
+// Add new Equipment type ( free / paid )
+// ==========================================================
+
+$('.new-equipment-type input[type="radio"]').change(function() {
+
+  if ($(this).val() == "Equipment has flat price") {
+    $(".new-equipment-price-container").slideDown("fast").find('input').prop('disabled', false);
+
+  } else {
+    $(".new-equipment-price-container").slideUp("fast").find('input').prop('disabled', true);
+  }
+});
+
+
 
 // Time Input field
 // ==========================================================
@@ -164,34 +178,34 @@ $('button.collapse').click(function(e){
 
 var timePickers = document.getElementsByClassName('timepicker-work');
 
-  for ( var i = 0; i < timePickers.length; i++) { 
-    var timeField = timePickers[i].dataset.time;
-    if(!timeField){
+for (var i = 0; i < timePickers.length; i++) {
+  var timeField = timePickers[i].dataset.time;
+  if (!timeField) {
+    timeField = '7:00am'
+  } else {
+    if (timeField == '') {
       timeField = '7:00am'
-    }else{
-      if(timeField == ''){
-        timeField = '7:00am'
-      }
     }
-    $(timePickers[i]).timepicker({
-      timeFormat: 'h:mm p',
-      interval: 30,
-      minTime: '7',
-      maxTime: '10:00pm',
-      defaultTime: timeField,
-      startTime: '7:00am',
-      dynamic: false,
-      dropdown: true,
-      scrollbar: true
-    });
+  }
+  $(timePickers[i]).timepicker({
+    timeFormat: 'h:mm p',
+    interval: 30,
+    minTime: '7',
+    maxTime: '10:00pm',
+    defaultTime: timeField,
+    startTime: '7:00am',
+    dynamic: false,
+    dropdown: true,
+    scrollbar: true
+  });
 }
 
 
 
-$('.day-working .day-off').change(function(){
-  if($(this).prop('checked')){
+$('.day-working .day-off').change(function() {
+  if ($(this).prop('checked')) {
     $(this).parent().parent().find('.timepicker-work').prop('disabled', true);
-  }else{
+  } else {
     $(this).parent().parent().find('.timepicker-work').prop('disabled', false);
   }
 
@@ -201,23 +215,20 @@ $('.day-working .day-off').change(function(){
 // ==========================================================
 
 
-$('.credential-option').change(function(){
+$('.credential-option').change(function() {
 
-  var chatchCheckBox =  $(this).attr('id');
-  var propertyContainer = '.new-'+chatchCheckBox+'-container';
-  var chatchNewInputField = '.new-'+chatchCheckBox;
+  var chatchCheckBox = $(this).attr('id');
+  var propertyContainer = '.new-' + chatchCheckBox + '-container';
+  var chatchNewInputField = '.new-' + chatchCheckBox;
 
+  if ($(this).prop('checked')) {
 
-
-
-  if( $(this).prop('checked')){
-
-    $(propertyContainer).slideDown( "fast" );
+    $(propertyContainer).slideDown("fast");
     $(chatchNewInputField).prop('disabled', false);
 
-  }else{
+  } else {
 
-    $(propertyContainer).slideUp( "fast" );
+    $(propertyContainer).slideUp("fast");
     $(chatchNewInputField).prop('disabled', true);
 
   }
@@ -227,17 +238,28 @@ $('.credential-option').change(function(){
 // Truck details 
 // ==========================================================
 
-$('#has-truck').change(function(){
-  if($(this).prop('checked')){
-    $( this ).parent().parent().find('textarea').slideDown("fast");
-    $( this ).parent().parent().find('textarea').prop('disabled', false);
-  }else{
-    $( this ).parent().parent().find('textarea').slideUp("fast");
-    $( this ).parent().parent().find('textarea').prop('disabled', true);
+$('#has-truck').change(function() {
+  if ($(this).prop('checked')) {
+    $(this).parent().parent().find('textarea').slideDown("fast").prop('disabled', false);
+  } else {
+    $(this).parent().parent().find('textarea').slideUp("fast").prop('disabled', true);
   }
 })
 
 
+// licenses select
+// ==========================================================
+
+$('select.licence-has-input').change(function() {
+
+  if ($(this).val() == "yes") {
+    $(this).next('input').slideDown('fast').prop('disabled', false);
+
+  } else {
+    $(this).next('input').slideUp('fast').prop('disabled', true);
+  }
+
+})
 
 
 // Testimonials Slider
@@ -324,14 +346,14 @@ $('#service-provider-slider').slick({
         slidesToShow: 1,
         centerPadding: '40px',
       }
-    },{
+    }, {
       breakpoint: 480,
       settings: {
         centerMode: true,
         slidesToShow: 1,
         centerPadding: '35px',
       }
-    },{
+    }, {
       breakpoint: 444,
       settings: {
         centerMode: false,
@@ -342,4 +364,3 @@ $('#service-provider-slider').slick({
 
   ]
 });
-
